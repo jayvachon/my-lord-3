2e8b9d2a07e01ab617098c4588cdc6e9a910e05b
jay <jayvachon@pop-os.localdomain>
2021-03-30 18:58:01 -0400

    notifications

---
+++ Assets/Scripts/Notification.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Notification : MB
+{
+
+	public GameObject panel;
+	public Text text;
+
+	void Start() {
+		Show("this is a great test");
+	}
+
+    public void Show(string message) {
+    	text.text = message;
+    	panel.SetActive(true);
+    	Co.WaitForSeconds(5, Hide);
+    }
+
+    public void Hide() {
+    	panel.SetActive(false);
+    }
+}
